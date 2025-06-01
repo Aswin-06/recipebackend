c63b0d3a5f453e78fac006e1d1f4d9019f92435e
Aswin-06 <satha61977@gmail.com>
2025-06-01 08:34:44 +0530

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,7 +1,7 @@
 const admin = require("firebase-admin");
 require('dotenv').config();
-const serviceAccount = JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS_JSON);
-serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
+const raw = process.env.GOOGLE_APPLICATION_CREDENTIALS_JSON.replace(/\\n/g, '\n');
+const serviceAccount = JSON.parse(raw);
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
